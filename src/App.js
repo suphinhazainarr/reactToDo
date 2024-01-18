8368bad605497b2f726589327a7176cad66167a3
suphinhazainarr <118817290+suphinhazainarr@users.noreply.github.com>
2024-01-19 01:06:01 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import React, { useState } from 'react';
-import Counter from './Counter';
+import React from 'react';
+import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const [state, setState] = useState(false);
-  let obj = {
-    name: 'suphin',
-    age: 20
-  };
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState('');
+
+  // const addTodo = () => {
+  //   if (toDo.trim() !== '') {
+  //     setToDos([...toDos, toDo]);
+  //     setToDo('');
+  //   }
+  // };
 
   return (
-    <div>
-      <h1 onClick={() => setState(!state)}>Show / Hide</h1>
-      {state ? <Counter {...obj} /> : null}
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input
+          value={toDo}
+          onChange={(e) => setToDo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i onClick={()=>setToDos([...toDos,{ text: toDo,status: false}])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {toDos.map((value, index) => (
+          <div key={index} className="todo">
+            <div className="left">
+              <input value={value.status} type="checkbox" name="" id="" />
+              <p>{value.text}</p>
+            </div>
+            <div className="right">
+              <i className="fas fa-times"></i>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
